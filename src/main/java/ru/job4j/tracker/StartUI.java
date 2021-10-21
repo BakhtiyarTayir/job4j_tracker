ad14e7722f8815ec6de466cdbd60a79ee1599ad4
bakhtiyartayirov <bakhtiyartayirov@gmail.com>
2021-10-21 16:54:56 +0500

    6. Date. Отображение даты. [#310017]

---
+++ src/main/java/ru/job4j/tracker/StartUI.java
@@ -0,0 +1,12 @@
+package ru.job4j.tracker;
+
+import java.time.LocalDateTime;
+import java.time.format.DateTimeFormatter;
+
+public class StartUI {
+    Item item = new Item();
+    LocalDateTime localDateTime = item.getCreated();
+    DateTimeFormatter formatter = DateTimeFormatter.ofPattern("dd-MMMM-EEEE-yyyy HH:mm:ss");
+    String currentDateTime = localDateTime.format(formatter);
+    System.out.println("currentDateTime");
+}
