1d3d7264eca0dfc4ff78cb8f164537d43c5162d9
bakhtiyartayirov <bakhtiyartayirov@gmail.com>
2021-10-22 14:52:47 +0500

    6. Date. Отображение даты. [#310017 #218905] исправление

--- src/main/java/ru/job4j/tracker/Item.java
+++ src/main/java/ru/job4j/tracker/Item.java
@@ -5,11 +5,6 @@ import java.time.LocalDateTime;
 public class Item {
     private int id;
     private String name;
-
-    public LocalDateTime getCreated() {
-        return created;
-    }
-
     private LocalDateTime created = LocalDateTime.now();
 
     public Item(){}
@@ -21,6 +16,10 @@ public class Item {
     public Item(int id, String name){
         this.id = id;
         this.name = name;
+    }
+
+    public LocalDateTime getCreated() {
+        return created;
     }
 
     public int getId() {
