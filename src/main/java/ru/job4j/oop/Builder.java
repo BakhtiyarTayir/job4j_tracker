24c6ae3ee0541ee1f65ab3d04b0cd9efd27b54a9
bakhtiyartayirov <bakhtiyartayirov@gmail.com>
2021-10-23 14:06:35 +0500

    3. Реализация профессий в коде [#6837 #218849]

--- src/main/java/ru/job4j/oop/Builder.java
+++ src/main/java/ru/job4j/oop/Builder.java
@@ -1,14 +1,15 @@
 package ru.job4j.oop;
 
 public class Builder extends Engineer{
-    private int Draw;
+    private int draw;
 
-    public Builder(String name, String surname, String education, int birthday, int certificate) {
+    public Builder(String name, String surname, String education, int birthday, int certificate, int draw) {
         super(name, surname, education, birthday, certificate);
+        this.draw = draw;
     }
 
     public int getDraw() {
-        return Draw;
+        return draw;
     }
 
     public Plan drawHousePlan(){return null;}
