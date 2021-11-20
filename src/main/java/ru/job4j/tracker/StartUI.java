9755a921cb1f971b7acab958a0b8c3fd569b1eb0
bakhtiyartayirov <bakhtiyartayirov@gmail.com>
2021-11-20 14:04:00 +0500

    9. toString [#310018]

--- src/main/java/ru/job4j/tracker/StartUI.java
+++ src/main/java/ru/job4j/tracker/StartUI.java
@@ -10,5 +10,6 @@ public class StartUI {
         DateTimeFormatter formatter = DateTimeFormatter.ofPattern("dd-MMMM-EEEE-yyyy HH:mm:ss");
         String currentDateTime = localDateTime.format(formatter);
         System.out.println(currentDateTime);
+        System.out.println(item);
     }
 }
