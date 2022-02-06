0746594029b4e68227cb9c40551e173162ccc419
bakhtiyartayirov <bakhtiyartayirov@gmail.com>
2022-02-06 23:35:08 +0500

    3. Интерфейс Input [#181092]

---
+++ src/main/java/ru/job4j/poly/Bus.java
@@ -0,0 +1,18 @@
+package ru.job4j.poly;
+
+public class Bus implements Transport{
+    @Override
+    public void go() {
+        System.out.println("Go");
+    }
+
+    @Override
+    public void passengers(int number) {
+        System.out.println("Passengers: " + number);
+    }
+
+    @Override
+    public double tankUp(int price) {
+        return 0;
+    }
+}
