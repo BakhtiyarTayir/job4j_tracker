74b5b9dfb6e95ee84412a95e10e74f7b16a799c5
bakhtiyartayirov <bakhtiyartayirov@gmail.com>
2022-02-16 21:08:31 +0500

    StartUI updated

--- src/main/java/ru/job4j/tracker/StartUI.java
+++ src/main/java/ru/job4j/tracker/StartUI.java
@@ -2,18 +2,6 @@ package ru.job4j.tracker;
 
 
 public class StartUI {
-    public static void findItemById(Input input, Tracker tracker) {
-        System.out.println("=== Find item by id ===");
-        int id = input.askInt("Enter id: ");
-        Item item = tracker.findById(id);
-        if (item != null) {
-            System.out.println(item);
-        } else {
-            System.out.println("Заявка с введенным id: " + id + " не найдена.");
-        }
-    }
-
-
     public void init(Input input, Tracker tracker, UserAction[] actions) {
         boolean run = true;
         while (run) {
