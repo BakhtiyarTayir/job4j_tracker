22f4ebeba2b52e36d7442d67e4398abd20d2060e
bakhtiyarTayir <bakhtiyarTayir@gmail.com>
2025-01-03 23:52:29 +0500

    4. Принципы раннего возврата и охранных выражений [#504875 #377413] updated

--- src/main/java/ru/job4j/early/PasswordValidator.java
+++ src/main/java/ru/job4j/early/PasswordValidator.java
@@ -1,11 +1,11 @@
 package ru.job4j.early;
 
-
 public class PasswordValidator {
+
     private static final String[] FORBIDDEN = {"qwerty", "12345", "password", "admin", "user"};
 
     public static String validate(String password) {
-        if (password == null ) {
+        if (password == null) {
             throw new IllegalArgumentException(
                     "Password can't be null"
             );
@@ -27,16 +27,16 @@ public class PasswordValidator {
         boolean hasDigit = false;
         boolean hasSpecial = false;
         for (char symbol : password.toCharArray()) {
-            if(Character.isUpperCase(symbol)) {
+            if (Character.isUpperCase(symbol)) {
                 hasUpCase = true;
             }
-            if(Character.isLowerCase(symbol)) {
+            if (Character.isLowerCase(symbol)) {
                 hasLowCase = true;
             }
-            if(Character.isDigit(symbol)) {
+            if (Character.isDigit(symbol)) {
                 hasDigit = true;
             }
-            if(!Character.isLetterOrDigit(symbol)) {
+            if (!Character.isLetterOrDigit(symbol)) {
                 hasSpecial = true;
             }
         }
