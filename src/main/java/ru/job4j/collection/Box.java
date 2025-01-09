d19121f4e86d2933926b54694a59ffb04f9e1c4e
bakhtiyarTayir <bakhtiyarTayir@gmail.com>
2025-01-09 09:40:07 +0500

    3. Автоупаковка / автораспаковка [#504971 #527639]

---
+++ src/main/java/ru/job4j/collection/Box.java
@@ -0,0 +1,15 @@
+package ru.job4j.collection;
+
+public class Box {
+    public static void main(String[] args) {
+        Integer n = 250;
+        Integer m = 250;
+
+        System.out.println(n == m);
+
+        Integer v = 127;
+        Integer w = 127;
+
+        System.out.println(v == w);
+    }
+}
