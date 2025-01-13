7107cd43c7fb550f08949bf27b09c7af0f849e4e
bakhtiyarTayir <bakhtiyarTayir@gmail.com>
2025-01-13 12:14:44 +0500

    0. Map, HashMap [#212652 #528193]

--- src/main/java/ru/job4j/collection/UsageMap.java
+++ src/main/java/ru/job4j/collection/UsageMap.java
@@ -5,11 +5,14 @@ import java.util.HashMap;
 public class UsageMap {
     public static void main(String[] args) {
         HashMap<String, String> hashMap = new HashMap<>();
+        hashMap.put("parsentev@yandex.ru", "Petr Arsentev");
+        hashMap.put("john@gmail.com", "John Doe");
+        hashMap.put("john@gmail.com", "John Doe2");
         hashMap.put("bakhtiyarTayirov@gmail.com", "Bakhtiyar Tayirov");
 
         for (String key : hashMap.keySet()) {
             String value = hashMap.get(key);
-            System.out.println("Key: " + key);
+            System.out.print("Key: " + key + " - ");
             System.out.println("Value: " + value);
         }
     }
