8cdbcb2148ae9ded2aac6bb6db317a8a187c6231
bakhtiyarTayir <bakhtiyarTayir@gmail.com>
2025-06-17 12:01:08 +0500

    3. Генерация документации по JavaDoc [#313880]

---
+++ src/main/java/ru/job4j/search/package-info.java
@@ -0,0 +1,11 @@
+/**
+ *  В этом пакете расположены классы, созданные при выполнении задания
+ *  "4. Телефонный справочник на базе ArrayList"
+ *  <p>Пакет дополнен классом PriorityQueue</p>
+ * @since 17.06.25
+ * @author Tayirov Bakhtiyar
+ * @version 1.0
+ */
+
+@Deprecated
+package ru.job4j.search;
